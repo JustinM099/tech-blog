68754122ee0b8e86826cd766aa8285e255f215c4
JustinM099 <justinm099@mac.com>
2022-01-01 22:24:56 -0800

    fixed edit function & seeds passwords

--- public/js/edit.js
+++ public/js/edit.js
@@ -2,16 +2,16 @@
 const editFunction = async (e) => {
     console.log('TIME TO EDIT')
     e.preventDefault()
-    const postTitle = document.querySelector('input[name="title"]').value
-    const postDescription = document.querySelector('input[name="post"]').value
+    const title = document.querySelector('input[name="title"]').value
+    const description = document.querySelector('input[name="post"]').value
     const route = window.location.pathname
     console.log(route)
 
-    const res = await fetch(`/${route}`, {
+    const res = await fetch(`${route}`, {
         method: 'PUT',
         body: JSON.stringify({
-            postTitle,
-            postDescription
+            title,
+            description
         }),
         headers: {
             'Content-Type': 'application/json'
